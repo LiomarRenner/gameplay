3e83b2ddd1f7a7920089c7171b1e34d549098930
LiomarRenner <liomarrenner@gmail.com>
2021-07-21 18:23:45 -0300

    done: SignIn and ButtonIcon
    add: touchableOpacityProps

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -32,7 +32,10 @@ export function SignIn() {
                     favoritos com seus amigos
                 </Text>
 
-                <ButtonIcon />
+                <ButtonIcon 
+                    title="Entrar com Discord"
+                    activeOpacity={0.7}
+                />
             </View>
         </View>
     );
