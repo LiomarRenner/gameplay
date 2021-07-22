60ed278424873d8526cf650c292c99bbe8135e36
LiomarRenner <liomarrenner@gmail.com>
2021-07-22 19:26:30 -0300

    feat: Update Avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { View, Image } from 'react-native';
 
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
 
-export function Avatar() {
-  return (
-    <View style={styles.container}>
+type ImageProps = {
+  urlImage: string;
+}
 
-    </View>
+export function Avatar({ urlImage } : ImageProps) {
+  const { secondary50, secondary70 } = theme.colors;
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[secondary50, secondary70]}
+    >
+      <Image 
+        source= {{ uri: urlImage }}
+        style={styles.avatar}
+      />
+    </LinearGradient>
   );
 }
