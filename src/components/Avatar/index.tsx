22a632ea8d8bc5fa16f5c9b1b693fbac27394678
LiomarRenner <liomarrenner@gmail.com>
2021-07-22 17:23:01 -0300

    feat: Starting Avatar Component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
+
+export function Avatar() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
