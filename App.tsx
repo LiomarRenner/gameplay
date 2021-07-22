2b5cb35e47b36860683e66468f5b1c50704a6d14
LiomarRenner <liomarrenner@gmail.com>
2021-07-22 11:06:35 -0300

    feat: add useFonts and remove stylesheet

--- App.tsx
+++ App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { useFonts } from 'expo-font';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
+
 import { SignIn } from './src/screens/SignIn'
-import { StyleSheet, View } from 'react-native';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Rajdhani_700Bold,
+    Rajdhani_500Medium,
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if(!fontsLoaded)
+    return <AppLoading />
+
   return (
-    <View style={styles.container}>
-      <SignIn />
-    </View>
+    <SignIn />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+
