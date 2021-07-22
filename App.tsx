9dea34b5376fb51495394b9d591d0b3bc950ec1d
LiomarRenner <liomarrenner@gmail.com>
2021-07-22 15:08:30 -0300

    change: Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import AppLoading from 'expo-app-loading';
 
 import { SignIn } from './src/screens/SignIn'
+import { Background } from './src/components/Background';
 
 
 export default function App() {
@@ -19,7 +21,14 @@ export default function App() {
     return <AppLoading />
 
   return (
-    <SignIn />
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
+    </Background>
   );
 }
 
