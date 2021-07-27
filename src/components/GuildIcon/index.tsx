d2158da5f9f6eaa41c792f0d0e1c0d8f09073ca8
LiomarRenner <liomarrenner@gmail.com>
2021-07-27 11:38:06 -0300

    feat: Create GuildIcon Component

---
+++ src/components/GuildIcon/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './styles';
+
+
+export function GuildIcon() {
+  return(
+    <View style={styles.container}>
+
+    </View>
+  );
+}
