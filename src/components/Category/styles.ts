9b7727836c5ffe657da983663112dd7ae9ab346b
LiomarRenner <liomarrenner@gmail.com>
2021-07-27 18:42:25 -0300

    fix:  Styles Category

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -13,26 +13,27 @@ export const styles = StyleSheet.create({
     content: {
         width: 100,
         height: 116,
-        backgroundColor: theme.colors.secondary40,
-        justifyContent: 'space-between',
         alignItems:'center',
         borderRadius: 8,
-        paddingVertical: 7
+        justifyContent: 'space-between',
+        paddingVertical: 20
     },
     checked: {
         width: 12,
         height: 12,
+        position: 'absolute',
+        top: 7,
+        right: 7,
         backgroundColor:theme.colors.primary,
-        alignSelf: 'flex-end',
-        marginRight: 7,
         borderRadius: 3
     },
     check: {
         width: 13,
         height: 13,
         backgroundColor:theme.colors.secondary100,
-        alignSelf: 'flex-end',
-        marginRight: 7,
+        position: 'absolute',
+        top: 7,
+        right: 7,
         borderColor: theme.colors.secondary50,
         borderWidth: 2,
         borderRadius: 3
