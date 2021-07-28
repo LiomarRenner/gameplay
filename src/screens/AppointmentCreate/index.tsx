79a8b4e635b8880f4078974b4bcb50fc5e7f8c4f
LiomarRenner <liomarrenner@gmail.com>
2021-07-28 19:24:55 -0300

    feat: Starting AppointmentCreate Screen

---
+++ src/screens/AppointmentCreate/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { 
+  Text, 
+  View 
+} from 'react-native';
+
+import { Background } from '../../components/Background';
+import { Header } from '../../components/Header';
+
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
+
+export function AppointmentCreate() {
+
+  return (
+    <Background>
+      <Header
+        title="Agendar Partida"
+      />
+    </Background>
+  );  
+}
+
