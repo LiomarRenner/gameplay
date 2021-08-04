d3bfd211599eee6567410ba4b3b5b20ced939887
LiomarRenner <liomarrenner@gmail.com>
2021-08-04 10:53:11 -0300

    fix: Add isCentered

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -77,7 +77,7 @@ export function AppointmentDetails() {
             data={item}
           />
         )}
-        ItemSeparatorComponent={() => <ListDivider />}
+        ItemSeparatorComponent={() => <ListDivider isCentered />}
         style={styles.members}
       />
 
