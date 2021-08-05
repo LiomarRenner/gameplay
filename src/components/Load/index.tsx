6e7084c46b101a0b25b434da8628c81da8340e86
LiomarRenner <liomarrenner@gmail.com>
2021-08-05 11:20:35 -0300

    update: Load Components

--- src/components/Load/index.tsx
+++ src/components/Load/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
 
+import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 
 export function Load(){
   return (
-    <View />
+    <View style={styles.container}>
+      <ActivityIndicator
+        size="large"
+        color={theme.colors.primary}
+      />
+    </View>
   );
 }
 
